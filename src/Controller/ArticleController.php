7887bb5caa47a1e4ae72637186d4f9753c67d976
Sandra <oawx72@gmail.com>
2020-10-13 17:02:58 +0200

    travail du 13/10/2020

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -13,5 +14,14 @@ class ArticleController extends AbstractController
     public function index()
     {
         return $this->render("articles/index.html.twig");
+    }
+
+    /**
+     * @Route("/article/{article}",name="article_show")
+     */
+    public function show(Article $article) {
+        return $this->render("articles/show.html.twig", [
+            "article" => $article
+        ]);
     }
 }
