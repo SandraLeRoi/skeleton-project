fa9d5a308d4108bb08241b3bde49c51fc4ef540f
Sandra <oawx72@gmail.com>
2020-10-13 12:29:37 +0200

    création nouveau projet avec ajout des packages

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ArticleController extends AbstractController
+{
+    /**
+     * @Route("/article", name="article")
+     */
+    public function index()
+    {
+        return $this->render("articles/index.html.twig");
+    }
+}
